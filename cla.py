db2d8b546e1220996e7cf649bc7c4e24426c67e1
Jenny Shi <jennpeare@gmail.com>
2014-05-02 00:35:59 -0400

    update

--- cla.py
+++ cla.py
@@ -1,58 +1,101 @@
+#from Crypto.Hash import SHA
+#from Crypto.PublicKey import RSA
 from flask import Flask, render_template, request
 from OpenSSL import SSL
-import string, random
+import string, random, requests
 
 #cla use private key
 
+# SSL setup
 context = SSL.Context(SSL.SSLv23_METHOD)
 context.use_privatekey_file("./keys/key.pem")
 context.use_certificate_file("./keys/cert.pem")
 
 cla = Flask(__name__)
 
+# list of eligible voters
 eligible_voters = { "100" : ["Jenny", "Shi", False],
                     "200" : ["Vicki", "Shen", False],
-                    "300" : ["Bob", "Builder", False] }
+                    "300" : ["Steph", "Yang", False],
+                    "400" : ["Grace", "Chi", False],
+                    "500" : ["Janice", "Tsai", False],
+                    "600" : ["Yamini", "Kathari", False],
+                  }
+
+# stores CSRF token
+session = {}
 
 @cla.route("/")
 def main():
-    return render_template("register.html")
+    return render_template("cla_register.html")
 
 @cla.route("/validation", methods=["POST"])
 def validation():
     if request.method == "POST":
-        message = validate_voters(request.form["first_name"], 
-                request.form["last_name"], request.form["pwd"])
+        return render_template("cla_validation.html",
+          message = validate_voters(request.form["first"],
+                                    request.form["last"],
+                                    request.form["secret"]))
 
+def send_to_ctf(validation_num):
+    #h = SHA.new(validation_num).digest() # encryption
+    #f = open("./keys/rsa", "r")
+    #key = RSA.importKey(f.read())
+    #sig = key.sign(h, key)
+    pair = {"validation_num" : validation_num }
+    req = requests.post("https://0.0.0.0:4321/add_voter", pair, verify=False)
 
-def generate_validation_number():
-    lst = [random.choice(string.ascii_letters + string.digits) for n in xrange(30)]
+def generate_valid_num(length):
+    lst = [random.choice(string.ascii_letters + string.digits)
+      for n in xrange(length)]
     rand = "".join(lst)
     return rand
 
-def validate_voters(first_name, last_name, pwd):
-    eligible = False
-    repeat = False
+def generate_random_str():
+    lst = [random.choice(string.ascii_letters + string.digits)
+      for n in xrange(30)]
+    rand = "".join(lst)
+    return rand
+
+def validate_voters(first, last, secret):
+    eligible = False # whether or not voter is eligible to register
+    repeat = False # whether voter has already registered
     validation_num = None
-    if not first_name or not last_name or not pwd:
-        return "Bad! You didn't fill out the form!"
-    elif eligible_voters.has_key(pwd):
-        voter = eligible_voters[pwd]
-        if voter[0] == first_name and voter[1] == last_name:
+    if not first or not last or not secret:
+        return "Please fill all of the fields."
+    elif eligible_voters.has_key(secret):
+        voter = eligible_voters[secret]
+        if voter[0] == first and voter[1] == last:
             if voter[2] == False:
-                validation_num = generate_validation_number()
-                voter.append(validation_num)
-                voter[2] = True
+                validation_num = generate_valid_num(15)
+                voter.append(validation_num) # add validation num to voter
+                send_to_ctf(validation_num) # send validation number to CTF
+                voter[2] = True # update: voter has alredy registered
                 eligible = True
             else:
                 repeat = True
-    
+
     if eligible == True and validation_num is not None:
-        return first_name + ", your validation number is: " + validation_num
+        return first + " " + last + ", your validation number is: " + validation_num
     elif repeat == True:
         return "You have already registered."
     else:
-        return "You're not eligible to vote."
+        return "You are not eligible to register."
+
+# CSRF check before each request
+@cla.before_request
+def csrf_protect():
+    if request.method == "POST":
+        token = session["csrf_token"]
+        if not token or token != request.form["csrf_token"]:
+            return "CSRF"
+
+def generate_csrf_token():
+    if "csrf_token" not in session:
+        session["csrf_token"] = generate_random_str()
+    return session["csrf_token"]
+
+cla.jinja_env.globals["csrf"] = generate_csrf_token() #global csrc token
 
 if __name__ == "__main__":
     cla.run(host="0.0.0.0", port=1234, debug=True, ssl_context=context)
--- ctf.py
+++ ctf.py
@@ -4,15 +4,100 @@ import string, random
 
 # ctf use public key
 
+# SSL setup
 context = SSL.Context(SSL.SSLv23_METHOD)
 context.use_privatekey_file("./keys/key.pem")
 context.use_certificate_file("./keys/cert.pem")
 
 ctf = Flask(__name__)
 
+voters = {}
+validation_numbers = {} # list of eligible voters | if they've already voted
+votes = { "dem" : 0, "rep" : 0, "tea" : 0 }
+
+# stores CSRF token
+# session = {}
+
 @ctf.route("/")
 def main():
-    return "hai"
+    return render_template("ctf_vote.html")
+
+@ctf.route("/hai", methods=["POST", "GET"])
+def add_voter():
+    if request.method == "POST":
+        print "POST"
+    else:
+        print "GET"
+
+# @ctf.route("/add_voter", methods=["POST"])
+# def add_voter():
+#     if request.method == "POST":
+#         print "HEY YO" + request.form['validation_num']
+#         return request.form["validation_num"]
+
+# @ctf.route("/add_voter", methods=["POST"])
+# def add_voter():
+#     if request.method == "POST":
+#         valid_num = request.form["validation_num"]
+#         validation_numbers[valid_num] = False
+
+@ctf.route("/confirmation", methods=["POST"])
+def confirmation():
+    if request.method == "POST":
+        print "POST TO '/conf'"
+        return render_template("ctf_confirmation.html",
+          message = validate_voter(request.form["rand_id"],
+                                    request.form["valid_num"],
+                                    request.form["party"]))
+
+@ctf.route("/voter_list")
+def voter_list():
+    return str(validation_numbers) + " " + str(votes)
+
+def validate_voter(rand_id, valid_num, vote):
+    eligible = False
+    repeat = False
+
+    if not rand_id or not valid_num or not vote:
+        return "Please fill all of the fields."
+    elif voters.has_key(rand_id):
+        return "Your random identification number is already taken, please try again."
+    elif validation_numbers.has_key(valid_num):
+        if validation_numbers[valid_num] == False:
+            voters[rand_id] = [valid_num, vote] # store voting info
+            votes[vote] = votes[vote] + 1 # increase tally
+            validation_numbers[valid_num] = True # update: voter already voted
+            eligible = True
+        else:
+            repeat = True
+
+    if eligible == True:
+        return rand_id + ", thanks for voting for " + vote
+    elif repeat == True:
+        return "You have alredy voted."
+    else:
+        return "You are not registered to vote."
+
+def generate_random_str():
+    lst = [random.choice(string.ascii_letters + string.digits)
+      for n in xrange(30)]
+    rand = "".join(lst)
+    return rand
+
+# # CSRF check before each request
+# @ctf.before_request
+# def csrf_protect():
+#     if request.method == "POST":
+#         token = session["csrf_token"]
+#         if not token or token != request.form["csrf_token"]:
+#             return "CSRF"
+#
+# def generate_csrf_token():
+#     if "csrf_token" not in session:
+#         session["csrf_token"] = generate_random_str()
+#     return session["csrf_token"]
+#
+# ctf.jinja_env.globals["csrf"] = generate_csrf_token() #global csrc token
 
 if __name__ == "__main__":
     ctf.run(host="0.0.0.0", port=4321, debug=True, ssl_context=context)
